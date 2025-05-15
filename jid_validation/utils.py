2577dd0fe1e37b6302cc00378f7b5a1d99220ab8
Ilya Kalashnikov <ilya.kalashnikov@redsolution.ru>
2025-05-15 10:14:12 +0500

    Changing validation functions.

--- jid_validation/utils.py
+++ jid_validation/utils.py
@@ -1,34 +1,32 @@
-from .jid import JID, parse, prep_localpart, prep_host
+from .jid import JID, prep_localpart, prep_host
+
+
+def _validate_jid(value):
+    try:
+        return JID(value)
+    except:
+        return None
 
 
 def validate_jid(value):
-    try:
-        jid_obj = JID(tuple=parse(value))
-    except Exception as e:
-        return {'success': False, 'error_message': e}
+    jid = _validate_jid(value)
+    return None if jid is None else jid.full()
 
-    if jid_obj.user and jid_obj.host:
-        try:
-            full_jid = jid_obj.full()
-            return {"success": True, "full_jid": full_jid}
-        except:
-            pass
 
-    return {'success': False, 'error_message': 'Invalid JID.'}
+def validate_bare_jid(value):
+    jid = _validate_jid(value)
+    return None if jid is None else jid.userhost()
 
 
 def validate_localpart(value):
-
     try:
-        localpart = prep_localpart(value)
-        return {"success": True, "localpart": localpart}
-    except Exception as e:
-        return {'success': False, 'error_message': e}
+        return prep_localpart(value)
+    except:
+        return None
 
 
 def validate_host(value):
     try:
-        host = prep_host(value)
-        return {"success": True, "host": host}
-    except Exception as e:
-        return {'success': False, 'error_message': e}
+        return prep_host(value)
+    except:
+        return None
